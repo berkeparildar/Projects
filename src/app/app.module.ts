1c2744035a705842125c0f5ab5f3f83242491d9a
Berke Parıldar <94047726+berkeparildar@users.noreply.github.com>
2024-03-14 20:38:21 +0300

    Delete everything for redirect to new page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParticleBackgroundComponent } from './particle-background/particle-background.component';
-import { HeaderComponent } from './header/header.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ProjectViewComponent } from './project-view/project-view.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
 import { Routes, RouterModule } from '@angular/router';
-import { CvViewComponent } from './cv-view/cv-view.component';
 import {HttpClientModule} from "@angular/common/http";
-import { MainPageComponent } from './main-page/main-page.component';
-import { ProjectTileComponent } from './project-tile/project-tile.component';
-import { CertificatesComponent } from './certificates/certificates.component';
-import { CertificateTileComponent } from './certificate-tile/certificate-tile.component';
-import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: '', component: ProjectsComponent },
-  { path: 'certificates', component: CertificatesComponent },
-  { path: 'contact', component: ContactComponent },
+  { path: '', component: AppComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CertificateTileComponent,
-    ContactComponent,
-    CertificatesComponent,
     ParticleBackgroundComponent,
-    ProjectTileComponent,
-    MainPageComponent,
-    HeaderComponent,
-    ProjectsComponent,
-    ProjectViewComponent,
-    CvViewComponent,
   ],
   imports: [
     BrowserModule,
